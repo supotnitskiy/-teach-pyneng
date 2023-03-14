b8ad88f69bdb894ff7c684b20826590c03e0a697
backup user <backup@backup3.agtel.net>
2023-03-14 09:59:23 +0300

    task_4_5.py сделано. Начал task_4_6.py

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -21,10 +21,4 @@ ospf_route. Значения из строки ospf_route надо получи�
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
-template = """
-Prefix                {}
-AD/Metric             {}
-Next-Hop              {}
-Last update           {}
-Outbound Interface    {}
-"""
+
